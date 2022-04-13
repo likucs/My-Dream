f5a442626587fc132cd500c43eae4b5c54e3776d
Liku cs <83144124+likucs@users.noreply.github.com>
2022-04-13 14:56:24 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -149,7 +149,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('This Movie Not Found In DataBase Contact @PromotionMediator (admin) to add This Movie file📂')
+            k = await query.message.edit('This Movie Not Found In DataBase Contact @iAmLiKu1 (admin) to add This Movie file📂')
             await asyncio.sleep(10)
             await k.delete()
 
